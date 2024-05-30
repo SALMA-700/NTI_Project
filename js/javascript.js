3b5abdcf5d1df6a6ceac21f96b284edd822adb6d
SALMA-700 <117851104+SALMA-700@users.noreply.github.com>
2024-05-30 22:53:44 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+
+// // setInterval( clock ,1000)
+// let myDiv = document.getElementById('slider');
+// let myimg =['url("images/11.jpg" )', 'url("images/1.jpg" )' , 'url("images/2.jpg" )', 'url("images/3.jpg" )' ,'url("images/4.jpg" )','url("images/5.jpg" )','url("images/6.jpg" )','url("images/7.jpg" )'];
+// let counter = 0
+// function changeimgs(){
+//     myDiv.style.background= myimg[counter];
+//     counter++
+//     counter = counter % myimg.length
+//     console.log(counter);
+// }
+// //()
+// setInterval( changeimgs ,1000)
